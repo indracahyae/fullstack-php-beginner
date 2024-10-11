952fd8e40b71e53cd7230d7ea252ef5ab01fb9e3
indracahyae <indracahyae@gmail.com>
2024-10-11 20:09:40 +0700

    login logout

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 <h2>
     PHP Beginner by Kodehack
 </h2>
+
 <a href="products">Data Products</a>
+<form action="" method="post">
+    <button type="submit" name="login">LOGIN</button>
+</form>
+
 
 <?php
+    // login
+    session_start();
+
+    if (isset($_POST['login'])) {
+        $_SESSION["login"] = true;
+        $url = "php-beginner/products";
+        exit(header("Location:http://$_SERVER[HTTP_HOST]/$url"));
+    }
+
     require_once 'templates/footer.php';
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,35 @@
 <?php
 require_once __DIR__ . '/../connection.php';
 // header('Content-type: text/plain');
+
+session_start();
+if(!isset($_SESSION["login"])){
+    $_SESSION["login"] = false;
+}
+
+if (!$_SESSION["login"]) {
+    $url = "php-beginner";
+    exit(header("Location:http://$_SERVER[HTTP_HOST]/$url"));
+}
 ?>
 
 <!-- menu -->
-<a href="/php-beginner/products">Products</a> <br><br>
+<a href="/php-beginner/products">Products</a> <br>
+<?php
+if($_SESSION["login"]){
+    echo "
+        <form action='' method='post'>
+            <button type='submit' name='logout'>LOGOUT</button>
+        </form>
+    ";
+
+    if (isset($_POST['logout'])) {
+        // $_SESSION["login"] = false;
+        session_unset();
+        $url = "php-beginner";
+        exit(header("Location:http://$_SERVER[HTTP_HOST]/$url"));
+    }
+
+
+}
+?>
